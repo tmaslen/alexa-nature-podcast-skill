af7fe644845372f54dec6e646f4523b4f7d8be35
tmaslen <tom_maslen@hotmail.com>
2017-10-25 08:09:11 +0100

    table update now destroys data before reinjecting it

--- natureAlexaSkillPodcast__updateData/index.js
+++ natureAlexaSkillPodcast__updateData/index.js
@@ -6,43 +6,134 @@ AWS.config.region = "eu-west-1";
 
 const podcastEndpoint = "http://feeds.nature.com/nature/podcast/current";
 
-exports.handler = function ( event, context, callback ) {
-	// Start here
-	// every hour start the process
-	// download new copy of data
+const dynamodb = new AWS.DynamoDB();
 
-	const dynamodb = new AWS.DynamoDB();
+function createTable () {
+	return new Promise ( ( resolve, reject ) => {
 
-	xmlStringToJson( fs.readFileSync( "data.xml" ), ( err, result ) => {
-		
-		if ( err ) {
-			console.log( "Error with the data.xml file" );
-			return;
-		}
-		
-		result.rss.channel[ 0 ].item.forEach( ( podcast, i ) => {
+		const params = {
+			AttributeDefinitions: [{
+				AttributeName: "id", 
+				AttributeType: "S"
+			}, 
+			{
+				AttributeName: "url", 
+				AttributeType: "S"
+			}], 
+			KeySchema: [{
+				AttributeName: "id", 
+				KeyType: "HASH"
+			}, 
+			{
+				AttributeName: "url", 
+				KeyType: "RANGE"
+			}], 
+			ProvisionedThroughput: {
+				ReadCapacityUnits: 5, 
+				WriteCapacityUnits: 5
+			}, 
+			TableName: "naturePodcasts"
+		};
 
-			const params = {
-				Item: {
-					"id": {
-						S: i.toString()
+		dynamodb.createTable( params, ( err, data ) => {
+			if ( err ) {
+				reject( err );
+			}
+			console.log( data );
+			waitUntil( "tableExists" ).then( resolve );
+   		});
+
+	});
+}
+
+function deleteTable () {
+	return new Promise ( ( resolve, reject ) => {
+
+		const params = {
+			"TableName": "naturePodcasts"
+		};
+
+		dynamodb.deleteTable( params, ( err, data ) => {
+			if ( err ) {
+				reject( err );
+			}
+			console.log( data );
+   			waitUntil( "tableNotExists" ).then( resolve );
+   		});
+
+	});
+}
+
+function waitUntil ( eventName ) {
+	return new Promise ( ( resolve, reject ) => {
+		const params = {
+			"TableName": "naturePodcasts"
+		};
+		dynamodb.waitFor( eventName, params, ( err, data ) => {
+			if ( err ) {
+				reject( err );
+			} else {
+				console.log( data );
+				resolve();
+			}
+		});
+
+	});
+
+}
+
+function postData () {
+
+	return new Promise ( ( resolve, reject ) => {
+
+		xmlStringToJson( fs.readFileSync( "data.xml" ), ( err, result ) => {
+			
+			if ( err ) {
+				reject( err );
+				return;
+			}
+			
+			result.rss.channel[ 0 ].item.forEach( ( podcast, i ) => {
+
+				const params = {
+					Item: {
+						"id": {
+							S: i.toString()
+						}, 
+						"url": {
+							S: podcast.link[ 0 ]
+						}
 					}, 
-					"url": {
-						S: podcast.link[ 0 ]
-					}
-				}, 
-				ReturnConsumedCapacity: "TOTAL", 
-				TableName: "naturePodcasts"
-			};
+					ReturnConsumedCapacity: "TOTAL", 
+					TableName: "naturePodcasts"
+				};
 
-			dynamodb.putItem(params, function(err, data) {
-				if (err) console.log(err, err.stack);
-				else     console.log(data);
+				dynamodb.putItem( params, ( err, data ) => {
+					if ( err ) {
+						reject( err );
+					} else {
+						console.log( data );
+						resolve();
+					}
+				});
+
 			});
 
 		});
 
 	});
+
+}
+
+exports.handler = function ( event, context, callback ) {
+	// Start here
+	// every hour start the process
+	// download new copy of data
+
+	deleteTable()
+		.then( createTable )
+		.then( postData )
+		.catch( console.log );
 
 }
 	
